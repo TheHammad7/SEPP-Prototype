b696ee6e8e59ec62b0ff668fcda0fab7554e309a
Sh4dow15 <187567472+Sh4dow15@users.noreply.github.com>
2024-12-04 13:58:25 +0000

    Added Message Saving

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
+import MessageComponent from './components/MessageComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Student Smart Fridge</h1>
-      
+        <MessageComponent />
       </header>
     </div>
   );
