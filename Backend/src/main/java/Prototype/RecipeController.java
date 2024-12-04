9c78e3c7d48ddbcd65537362d08cbad0ec5ef4c3
Sh4dow15 <187567472+Sh4dow15@users.noreply.github.com>
2024-12-04 18:13:10 +0000

    Removed imports added by accident while experimenting

--- Backend/src/main/java/Prototype/RecipeController.java
+++ Backend/src/main/java/Prototype/RecipeController.java
@@ -2,21 +2,15 @@ package Prototype;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import static com.mongodb.client.model.Filters.all;
 @RestController
 @RequestMapping("/api/recipes")
 public class RecipeController {
