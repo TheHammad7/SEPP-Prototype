5800859087d1c985c8ecea0e260d4c8c3c2c64bb
Casey Shea <shea.casey05@gmail.com>
2024-12-07 14:59:51 +0000

    Add menu icon to the banner

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
 
+import MenuIcon from './assets/Mingcute Menu Fill.svg';
+
 function App() {
+
+  const handleMenuClick = (event) => {
+    event.currentTarget.blur(); // Removes focus from the button
+  };
 
   return (
 
     <div className="MacbookAir1">
       <div className="PageHeader"> 
         <div className='Banner'>
-          
+        <button className="Menu" onClick={handleMenuClick}>
+          <img src={MenuIcon} alt="Menu" className="Menu-icon" />
+        </button>
         </div>
       </div>
     </div>
