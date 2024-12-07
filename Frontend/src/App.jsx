9c6d2a5bad718a71e4744903e5391fadc3478ce8
Casey Shea <shea.casey05@gmail.com>
2024-12-07 15:36:10 +0000

    Add recipe app logo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 import MenuIcon from './assets/menu-icon.svg';
 import SSHLogo from './assets/ssh-logo.svg';
+import RecipeAppIcon from './assets/recipe-app-icon.svg'
 
 function App() {
 
@@ -12,14 +13,18 @@ function App() {
   return (
 
     <div className="MacbookAir1">
+
       <div className="PageHeader"> 
+
         <div className='Banner'>
         <button className="Menu" onClick={handleMenuClick}>
           <img src={MenuIcon} alt="Menu" className="Menu-icon" />
         </button>
         <img src={SSHLogo} alt="SSH Logo" className="SSHLogo" />
         </div>
+        <img src={RecipeAppIcon} alt="Recipe App Icon" className="RecipeIcon" />
       </div>
+
     </div>
   );
 
