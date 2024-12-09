d1bed13954eb68c93726e0a99d2b38f26f1ef8a0
Casey Shea <shea.casey05@gmail.com>
2024-12-09 22:38:59 +0000

    Add images to recipe cards

--- Frontend/src/components/RecipeCard.jsx
+++ Frontend/src/components/RecipeCard.jsx
@@ -4,9 +4,12 @@ import "./RecipeCard.css";
 import StarRatingIcon from '../assets/5-star-rating-icon.svg';
 import TimerIcon from '../assets/timer-icon.svg';
 
-const RecipeCard = ({ title, cookingTime }) => {
+const RecipeCard = ({ title, image, cookingTime }) => {
   return (
     <div className="RecipeCard">
+
+        <img src={image} alt={`${title} image`} className="RecipeImage" />
+
         <div className="RecipeCardInfo">
             <img src={StarRatingIcon} alt="5-Star-Rating-Icon" className="StarRatingIcon" />
             <img src={TimerIcon} alt="TimerIcon" className="TimerIcon" />
