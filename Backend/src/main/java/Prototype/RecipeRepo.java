8eddc6c98e35f2cfc1760948b786acb749525d98
gracejdaniels <danielsgrace05@gmail.com>
2024-12-09 18:09:40 +0000

    Remove unnecessary source files from Backend

--- Backend/src/main/java/Prototype/RecipeRepo.java
+++
@@ -1,9 +0,0 @@
-package Prototype;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepo extends MongoRepository<Recipes, ObjectId>{
-
-}
